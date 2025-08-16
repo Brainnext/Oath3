37dea91e35750f56fcc3351c4a28dbf44a4e85c3
Sirlinux <ainaemanuel@gmail.com>
2025-08-16 03:53:14 +0100

    Hardhat has been updated to include the monad testnet network

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    monadTestnet: {
+      url: "https://testnet-rpc.monad.xyz",
+      acccounts: [safely.env.PRIVATE_KEY],
+      chainId: 20143,
+    },
+  },
 };
